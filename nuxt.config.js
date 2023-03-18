4274ffcc11bacc8cc1032c6474a7602bd9511dbe
kofacts <obodugorapheal@gmail.com>
2023-03-18 01:13:31 +0100

    Dont do that

--- nuxt.config.js
+++ nuxt.config.js
@@ -4,7 +4,7 @@ export default {
 
   // Global page headers: https://go.nuxtjs.dev/config-head
   head: {
-    title: 'Home | MyOneFi',
+    title: 'Home - OwoFi',
     htmlAttrs: {
       lang: 'en'
     },
