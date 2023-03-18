7724aab658d183ca21d1432645199122963b236f
kofacts <obodugorapheal@gmail.com>
2023-03-18 01:03:38 +0100

    Dont do that

--- components/Layouts/Navbar.vue
+++ components/Layouts/Navbar.vue
@@ -6,8 +6,8 @@
 
         <ul class="flex lg:gap-12 gap-6 items-center">
             <li class="text-mini text-primary whitespace-pre text-sm"> <a href="#how-it-works">How it works</a></li>
-            <li class="text opacity-60 text-sm hidden lg:block"><router-link :to="{path: '/join'}"> Join Private List</router-link></li>
-            <li class="text opacity-60 text-sm"><a href="#footer">Socials</a></li>
+            <li class="text opacity-60 text-sm hidden text-primary text-mini lg:block"><router-link :to="{path: '/join'}"> Join Private List</router-link></li>
+            <li class="text opacity-60 text-sm text-primary text-mini"><a href="#footer">Socials</a></li>
         </ul>
     </div>
 </template>
