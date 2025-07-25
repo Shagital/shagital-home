23ef204532ac5d916a407602ce514a1e0a56f756
Rapheal clinton <clintonrapheal5@gmail.com>
2025-07-25 06:05:38 -0300

    firsst commit

---
+++ src/assets/asset.js
@@ -0,0 +1,44 @@
+  import projectImage from '../assets/projectImage.png'
+
+  export const projects = [
+    {id: 1,
+      projectName: "Owofi",
+      projectImage: projectImage
+    },
+    {id: 2,
+      projectName: "Owofi",
+      projectImage: projectImage
+    },
+    {id: 3,
+      projectName: "Owofi",
+      projectImage: projectImage
+    },
+    {id: 4,
+      projectName: "Owofi",
+      projectImage: projectImage
+    },
+    {id: 5,
+      projectName: "Owofi",
+      projectImage: projectImage
+    },
+    {id: 6,
+      projectName: "Owofi",
+      projectImage: projectImage
+    },
+    {id: 7,
+      projectName: "Owofi",
+      projectImage: projectImage
+    },
+    {id: 8,
+      projectName: "Owofi",
+      projectImage: projectImage
+    },
+    {id: 9,
+      projectName: "Owofi",
+      projectImage: projectImage
+    },
+    {id: 10,
+      projectName: "Owofi",
+      projectImage: projectImage
+    },
+  ]
