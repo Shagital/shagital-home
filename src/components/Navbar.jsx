b6acb8425b8d7a89bb0fcd2c8cd84f552049ee09
Rapheal clinton <clintonrapheal5@gmail.com>
2025-07-25 06:08:15 -0300

    nav

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -15,7 +15,7 @@ const Navbar = () => {
   return (
     <div className='m-4 md:mx-7 md:my-7 lg:mx-9 lg:my-9 text-white scroll-smooth'>
       <nav className='backdrop-blur-3xl bg-white/10 flex items-center justify-between rounded-full px-4 py-3 md:px-6'>
-        <img src="/src/Logo.png" alt="navbar-logo" className='w-8 rounded'/>
+        <img src="./src/Logo.png" alt="navbar-logo" className='w-8 rounded'/>
         <div className='hidden md:flex gap-6 lg:gap-12'>
           {navLinks.map((link)=>(
             <div key={link.name}>
