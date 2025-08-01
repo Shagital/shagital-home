2297498a764cd54dd65b1e4a091c3015a550cb12
Rapheal clinton <clintonrapheal5@gmail.com>
2025-08-01 15:15:42 +0100

    updated image

--- src/assets/asset.js
+++ src/assets/asset.js
@@ -8,42 +8,58 @@
   export const projects = [
     {id: 1,
       projectName: "Owofi",
-      projectImage: projectImage
+      projectImage: "https://shagital.com/_nuxt/img/owofi.3f7dc2f.jpeg",
+      ImageOverlay: "https://shagital.com/_nuxt/img/owofi.5430edd.png",
+      Desc: "Easy Offline Payment in Nigeria. Send and Receive Money even with Zero internet.",
+      hrefLink: "https://owofi.com/"
     },
     {id: 2,
       projectName: "Owofi",
-      projectImage: projectImage
+      projectImage: "https://shagital.com/_nuxt/img/netflix.6312df9.jpeg",
+      ImageOverlay: "https://shagital.com/_nuxt/img/mysub.35fe1c0.png",
+      Desc: "Sharing Subscriptions made easy. Easily Share services like Netflix, Spotify and more.",
+      hrefLink: "https://mysub.io/"
     },
     {id: 3,
-      projectName: "Owofi",
-      projectImage: projectImage
+      projectName: "VueJobs",
+      projectImage: "https://shagital.com/_nuxt/img/engineer.8c93d01.jpeg",
+      ImageOverlay: "https://shagital.com/_nuxt/img/vuejobs.c3e3404.png",
+      Desc: "Making Remote Vue jobs accesible to all. Detailedly curated from global companies.",
+      hrefLink: "google.com"
     },
     {id: 4,
-      projectName: "Owofi",
-      projectImage: projectImage
+      projectName: "feedmonkay",
+      projectImage: "https://shagital.com/_nuxt/img/customer.fbb1472.jpeg",
+      ImageOverlay: "https://shagital.com/_nuxt/img/feedmonkey.075190a.png",
+      Desc: "An easy way to connect with your customers. Seamless plug-n-play widget to your app.",
+      hrefLink: "https://feedmonkey.io/"
     },
     {id: 5,
-      projectName: "Owofi",
-      projectImage: projectImage
-    },
-    {id: 6,
-      projectName: "Owofi",
-      projectImage: projectImage
+      projectName: "TBVH",
+      projectImage: "https://shagital.com/_nuxt/img/customer.fbb1472.jpeg",
+      ImageOverlay: "https://shagital.com/_nuxt/img/tbvh.d92a718.png",
+      Desc: "An anonymous platform for the new generation to share, connect, and be heard.",
+      hrefLink: "https://tbvh.app/"
     },
     {id: 7,
-      projectName: "Owofi",
-      projectImage: projectImage
+      projectName: "Goal Genius",
+      projectImage: "https://shagital.com/_nuxt/img/customer.fbb1472.jpeg",
+      ImageOverlay: "https://shagital.com/_nuxt/img/goalgenius.9222f04.png",
+      Desc: "Get real-time live scores and expert AI-generated match predictions. Subscribe for exclusive access to in-depth analysis and forecasts for your favorite games.",
+      hrefLink: "https://goalgenius.app/"
     },
     {id: 8,
-      projectName: "Owofi",
-      projectImage: projectImage
+      projectName: "Trusthield",
+      projectImage: "https://shagital.com/_nuxt/img/customer.fbb1472.jpeg",
+      ImageOverlay: "https://shagital.com/_nuxt/img/trustshield.8ae119d.png",
+      Desc: "With TrustShield, businesses can blacklist fraudulent users and share that information across multiple startups, enabling them to better protect themselves and their customers.",
+      hrefLink: "https://trustshield.app/"
     },
-    {id: 9,
-      projectName: "Owofi",
-      projectImage: projectImage
-    },
-    {id: 10,
-      projectName: "Owofi",
-      projectImage: projectImage
+    {id: 6,
+      projectName: "Xpreview",
+      projectImage: "https://shagital.com/_nuxt/img/customer.fbb1472.jpeg",
+      ImageOverlay: "https://shagital.com/_nuxt/img/xpreview.d3a67cc.png",
+      Desc: "XPreview - Enhance your Twitter links with better preview images. Preview links before posting on Twitter.",
+      hrefLink: "https://xpreview.app/"
     },
   ]
